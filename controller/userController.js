aa5ac9e096948b546273e1f6af1153cf3e08304f
Ahraar Alam <ahraralam2012@gmail.com>
2022-09-26 23:49:57 +0530

    CRUD completed

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,32 @@
+const { response } = require("express");
 const userDetails = require("../modals/userSchema");
 
 class User {
   create = async (req, res) => {
-    const { name, age, address } = req.body;
-    const response = await userDetails.create({ name, age, address });
-    res.send(response);
+    try {
+      const { name, age, address } = req.body;
+      if (!name) {
+        throw {
+          message: "Please enter a name",
+        };
+      }
+      if(!age){
+         throw {
+           message: "Please enter a age",
+         };
+      }
+      const response = await userDetails.create({ name, age, address });
+      res.send({
+        status: true,
+        response: response,
+        message: "Successfully Created user",
+      });
+    } catch (error) {
+      res.send({
+        status: false,
+        response: error.message,
+      });
+    }
   };
 
   getUsers = async (req, res) => {
@@ -15,6 +37,19 @@ class User {
   getUser = async (req, res) => {
     const _id = req.query._id;
     const response = await userDetails.findOne({ _id: _id });
+    res.send(response);
+  };
+
+  updateUser = async (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const response = await userDetails.updateMany({ _id: id }, { name: name });
+    res.send(response);
+  };
+
+  deleteUser = async (req, res) => {
+    const id = req.body.id;
+    const response = await userDetails.deleteMany({ _id: id });
     res.send(response);
   };
 }
@@ -29,4 +64,5 @@ U - Update
 D - Delete
 
 CALLBACK HELL
+Migration
 */
--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,8 @@ const User = require("../controller/userController.js");
 router.post("/", User.create);
 router.get("/", User.getUsers);
 router.get("/user", User.getUser);
+router.put("/update", User.updateUser);
+router.delete("/delete", User.deleteUser);
 
 
 module.exports = router;
